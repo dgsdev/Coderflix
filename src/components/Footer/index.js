e1ffa251d51f1ad14908b76ea0fc135687b81f58
dgsdev <dgspramos@gmail.com>
2020-07-28 13:12:33 -0300

    first commit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,11 +4,11 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="https://github.com/dgsdev">
+        <img src="https://fontmeme.com/permalink/200728/7e61772242360ba60113458b31872879.png" alt="Logo Beccaflix"/>
       </a>
       <p>
-        Orgulhosamente criado durante a
+        Orgulhosamente criado por dgsdev durante a
         {' '}
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
